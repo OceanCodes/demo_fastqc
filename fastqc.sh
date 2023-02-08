3de52c287db0ded3413e628584efe85aad6c1168
jake-valsamis <107424756+jake-valsamis@users.noreply.github.com>
2023-02-08 17:34:48 -0500

    Add files via upload

---
+++ fastqc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#general syntax:
+#fastqc seqfile1 seqfile2 .. seqfileN
+
+for file in $(find -L ../data -name "*.fastq*"); do
+    echo "Running FastQC on $(basename -a $file)"
+    fastqc --outdir ../results $file &
+done
+wait
+
+echo "Finished with FastQC"
+
+#some change in GitHub
+#more changes
